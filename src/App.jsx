0818634e0344002a8810378532e9e3cd32e97e21
Shreysid2352 <Shreysid2352@gmail.com>
2024-03-06 23:00:58 +0530

    analytics

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,20 @@ import "./App.css";
 import Experience from "./components/Experience";
 import Configurator from "./components/Configurator";
 import { CustomizationProvider } from "./Contexts/Customization";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
-    <Analytics>
     <CustomizationProvider>
-    <div className="App">
-      <Canvas camera={{ position: [2, 2, 2], fov: 45, near: 3, far: 3 }}>
-        <color attach="background" args={["#213547"]} />
-        <Experience />
-      </Canvas>
-      <Configurator/>
-    </div>
+      <div className="App">
+        <Canvas camera={{ position: [2, 2, 2], fov: 45, near: 3, far: 3 }}>
+          <color attach="background" args={["#213547"]} />
+          <Experience />
+        </Canvas>
+        <Configurator />
+        <Analytics />
+      </div>
     </CustomizationProvider>
-    </Analytics>
   );
 }
 
